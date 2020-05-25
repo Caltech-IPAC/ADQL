d2ddd6417294543a5c7fbba04ba2d653d4cc5578
John C Good <jcg@ipac.caltech.edu>
2020-05-25 16:14:32 -0700

    Initial commit

---
+++ tests/test_adql.py
@@ -0,0 +1,42 @@
+import pytest
+
+from adql import ADQL
+from spatial_index import SpatialIndex
+
+
+infile  = open('tests/indata.txt',  'r')
+outfile = open('tests/outdata.txt', 'r')
+ 
+values = []
+
+while True:
+
+    adql_in = infile.readline().rstrip('\n')
+
+    if not adql_in:
+        break
+
+    sql_out = outfile.readline().rstrip('\n')
+    
+    if not sql_out:
+        break
+
+    values.append((adql_in, sql_out))
+
+
+
+@pytest.mark.parametrize("adqlstr,sqlstr", values)
+
+def test_adql(adqlstr,sqlstr):
+
+    adql = ADQL(level=20, colname='htm20', debugfile=None,
+               mode=SpatialIndex.HTM, encoding=SpatialIndex.BASE10)
+
+    try:
+        sqlnew = adql.sql(adqlstr)
+
+    except Exception as adql_error:
+
+        sqlnew = str(adql_error)
+
+    assert sqlnew == sqlstr
