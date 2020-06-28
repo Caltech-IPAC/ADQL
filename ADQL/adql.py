bcaa9f1e6826b0a6cf20c5d18efdf8ffb5bed04d
JohnGood <jcg@ipac.caltech.edu>
2020-06-28 11:01:24 -0700

    Add copyright.

--- ADQL/adql.py
+++ ADQL/adql.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2020, Caltech IPAC.
+# License information at
+
+#   https://github.com/Caltech-IPAC/nexsciTAP/blob/master/LICENSE
+
+
 import math
 import numpy as np
 import sqlparse
